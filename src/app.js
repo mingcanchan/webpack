ddf5c5a86d2b82dcc2d3a5a0192064cf59424132
mingcanchen <mingcanchen@hotmail.com>
2018-01-15 18:04:30 +0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import './css/common.css';
+import layer from './components/layer/layer.js';
+const App = function(){
+	const num = 1;
+	alert(num);
+	console.log(layer);
+}
+new App()
---
+++ webpack.dev.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const htmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: './src/app.js',
+  output: {
+	path: __dirname+'/dist',
+    filename: 'js/[name].bundle.js',
+
+  },
+  module: {
+	rules: [
+		{
+            test: /(\.jsx|\.js)$/,
+            use: {
+                loader: "babel-loader"
+            },
+            exclude: __dirname+'node_modules',
+			include: __dirname+'src'
+			
+        },
+		{
+			test: /\.css/,
+			use: [{
+				loader:'style-loader'
+			},{
+				loader:'css-loader'
+			},{ 
+				loader: 'postcss-loader',options: {
+					plugins: function () {return [require('autoprefixer')];}
+				}
+			}]
+		}
+	]
+  },
+	
+  plugins: [
+	new htmlWebpackPlugin({
+		filename: 'index.html',
+		template: 'index.html',
+		inject: 'body'		
+	})
+  ]
+};
